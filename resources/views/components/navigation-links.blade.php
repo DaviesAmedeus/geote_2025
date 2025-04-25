83b33df439785a736e3df3e1597bd3b74d263ed7
DaviesAmedeus <amedeusedgar@gmail.com>
2025-04-25 21:19:28 +0300

    created the landing page for geote_2025

---
+++ resources/views/components/navigation-links.blade.php
@@ -0,0 +1,45 @@
+<nav id="navbar" class="navbar">
+    <ul>
+         <li><a href="{{ route('home') }}">Home</a></li>
+       <li><a href="{{ route('about') }}">About</a></li>
+       <li><a href="{{ route('engagements') }}">Engagements</a></li>
+       {{-- <li><a href="{{ route('projects.index') }}">Projects</a></li> --}}
+        <li class="dropdown"><a href="#"><span>Blog</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+             <ul>
+               {{-- <li><a href="{{ route('blog.index') }}">Blog</a></li> --}}
+               {{-- <li><a href="{{ route('publications') }}">Publications</a></li> --}}
+             </ul>
+        </li>
+         <li class="dropdown"><a href="#"><span>Events</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+             <ul>
+               <li><a href="{{ route('geospark') }}">Geo_spark</a></li>
+               <li><a href="{{ route('mapathons') }}">Mapathons</a></li>
+             </ul>
+        </li>
+        <li class="dropdown"><a href="#"><span>Programs</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="{{ route('fpt') }}">FPT</a></li>
+              <li><a href="{{ route('shortcourses') }}">GIS Short course</a></li>
+              <li><a href="{{ route('mentorship') }}">GIS Mentorship</a></li>
+
+            </ul>
+       </li>
+        @if (optional(Auth::user())->is_admin)
+        <li class="dropdown"><a href="#"><span>LOGGED IN</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="/admin/dashboard">Dashboard</a></li>
+          </li>
+            </ul>
+       </li>
+        @endif
+
+        @if (optional(Auth::user())->is_admin===0)
+        <li class="dropdown"><a href="#"><span>LOGGED IN</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="/staff">Dashboard</a></li>
+          </li>
+            </ul>
+       </li>
+        @endif
+     </ul>
+ </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\WebsiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(WebsiteController::class)->group(function(){
+    Route::get('/',  'index')->name('home');
+    Route::get('/about','about')->name('about');
+    Route::get('/engagements',  'engagements')->name('engagements');
+
+    Route::get('/mapathons',  'mapathons')->name('mapathons');
+    Route::get('/geospark',  'geospark')->name('geospark');
+    Route::get('/fpt',  'fpt')->name('fpt');
+    Route::get('/shortcourses',  'shortcourses')->name('shortcourses');
+    Route::get('/mentorship',  'mentorship')->name('mentorship');
+
+
+
 });
