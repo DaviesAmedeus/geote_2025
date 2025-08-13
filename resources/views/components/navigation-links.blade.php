c5ef1b5da4f7e58b3ddb25be329e6b3fb98b409e
DaviesAmedeus <amedeusedgar@gmail.com>
2025-08-13 17:25:22 +0300

    fixed the mobile navigation

--- resources/views/components/navigation-links.blade.php
+++ resources/views/components/navigation-links.blade.php
@@ -1,40 +1,44 @@
 <nav id="navbar" class="navbar">
     <ul>
-         <li><a href="{{ route('home') }}">Home</a></li>
-       <li><a href="{{ route('about') }}">About</a></li>
-       <li><a href="{{ route('projects') }}">Projects</a></li>
-         {{-- <li class="dropdown"><a href="#"><span>Blog</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a> --}}
-             <ul>
-               {{-- <li><a href="{{ route('blog.index') }}">Blog</a></li> --}}
-               {{-- <li><a href="{{ route('publications') }}">Publications</a></li> --}}
-             </ul>
-        </li>
+        <li><a href="{{ route('home') }}" class="active">Home</a></li>
+        <li><a href="{{ route('about') }}">About</a></li>
+        <li><a href="{{ route('projects') }}">Projects</a></li>
+        {{-- <li class="dropdown"><a href="#"><span>Blog</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+        <ul>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Publications</a></li>
+        </ul>
+        </li> --}}
 
-         <li class="dropdown"><a href="#"><span>Events</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-             <ul>
-               <li><a href="{{ route('geospark') }}">Geo_spark</a></li>
-               <li><a href="{{ route('mapathons') }}">Mapathons</a></li>
-             </ul>
-        </li>
-        <li class="dropdown"><a href="#"><span>Programs</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+        <li class="dropdown"><a href="#"><span>Events</span> <i
+                    class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="{{ route('fpt') }}">FPT</a></li>
-              <li><a href="{{ route('shortcourses') }}">GIS Short course</a></li>
-              <li><a href="{{ route('mentorship') }}">GIS Mentorship</a></li>
+                <li><a href="{{ route('geospark') }}">Geo_spark</a></li>
+                <li><a href="{{ route('mapathons') }}">Mapathons</a></li>
+            </ul>
+        </li>
+        <li class="dropdown"><a href="#"><span>Programs</span> <i
+                    class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+                <li><a href="{{ route('fpt') }}">FPT</a></li>
+                <li><a href="{{ route('shortcourses') }}">GIS Short course</a></li>
+                <li><a href="{{ route('mentorship') }}">GIS Mentorship</a></li>
 
             </ul>
-       </li>
+        </li>
         @if (optional(Auth::user())->is_admin)
-        <li class="dropdown"><a href="#"><span>LOGGED IN</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="/admin/dashboard">Dashboard</a></li>
-          </li>
-            </ul>
-       </li>
-        @endif
+            <li class="dropdown"><a href="#"><span>LOGGED IN</span> <i
+                        class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="/admin/dashboard">Dashboard</a></li>
+            </li>
+    </ul>
+    </li>
+    @endif
 
-         <li><a class="btn btn-lg btn-outline-success px-3 py-2" href="/donate">Donate</a></li>
+    <li><a class="btn btn-lg btn-outline-success px-3 py-2" href="/donate">Donate</a></li>
 
 
-     </ul>
- </nav>
+    </ul>
+</nav>
+
