210cd577e41a61f0b2a23cc5c6a999763029ad73
DaviesAmedeus <amedeusedgar@gmail.com>
2026-03-14 07:48:30 +0300

    updated partners

--- resources/views/components/geote-partners.blade.php
+++ resources/views/components/geote-partners.blade.php
@@ -10,13 +10,13 @@
             <x-partner imglink='assets/img/partners/nemboyataifa.webp' title="United Republic of Tanzania" />
             <x-partner imglink='assets/img/partners/sua.png' title="Sokoine University (SUA)" />
             <x-partner imglink='assets/img/partners/ym.jpeg' title='Youth Mappers' />
+             <x-partner imglink='assets/img/partners/savethechildren.webp' title='Save The Children' />
             <x-partner imglink='assets/img/partners/tomtom2.png' title='TomTom' />
             <x-partner imglink='assets/img/partners/omdtzlogo.png' title='OMDTZ' />
             <x-partner imglink='assets/img/partners/mapbox.png' title='Mapbox' />
             <x-partner imglink='assets/img/partners/okf.webp' title='Open Knowledge Foundation' />
             <x-partner imglink='assets/img/partners/ramaniafrica.webp' title='Ramani Africa' />
             <x-partner imglink='assets/img/partners/rootgis.webp' title='Root GIS' />
-            <x-partner imglink='assets/img/partners/savethechildren.webp' title='Save The Children' />
             <x-partner imglink='assets/img/partners/openmappinghub.webp' title='Open Mapping Hub' />
             <x-partner imglink='assets/img/partners/mbtr.webp' title='My Birthday Tree Organization' />
 
